6ae9d69a16f1d5edcdc9d0574ee03c99139c4aed
MalliaGeiger <malliaggr@gmail.com>
2023-11-30 18:06:15 +0100

    get_image package to get image and copy it on coordinate file

--- forests/get_image/get_image.py
+++ forests/get_image/get_image.py
@@ -2,50 +2,35 @@
 import os
 import shutil
 from datetime import datetime
+import pandas as pd
 
 ## get to the right directory
-os.getcwd()
-os.chdir("..")
-root = os.getcwd()
-path = root + '/raw_data/ForestNetDataset/examples/'
+project_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-#########################################################################################################
-                            #creating a function in order to extract date
-#########################################################################################################
+data_dir = os.path.join(project_path, 'raw_data', 'ForestNetDataset')
 
-def extract_date(im_name):
-    # split im_name with '_' take element corresponding to date (here the first 3)
-    date = im_name.split('_')[:3]
-    # rejoin it to have get the date
-    return '_'.join(date)
+train = pd.read_csv(os.path.join(data_dir, 'train.csv'))
+train['dataset'] = 'train'
+# test =  pd.read_csv(os.path.join(data_dir, 'test.csv'))
+#test['dataset'] = 'test'
+# val = pd.read_csv(os.path.join(data_dir, 'val.csv'))
+#val['dataset'] = 'val'
+# dataset = pd.concat([train, test, val], axis=0)
+
+
+
 
 #########################################################################################################
             ## for loop to iterate on each file in 'example' and copy select file in each file
 #########################################################################################################
-
-for files in os.listdir(path):
-    ## set the path where we want to copy our image
-    output_dir = '/Users/malliageiger/code/malliaggr/forests_health_monitoring/raw_data/ForestNetDataset/test'#path +files
-    ## set the path to access or visible image
-    #get_image = output_dir +'/images/visible/'
-    get_image = path +files + '/images/visible/'
-    #print(get_image)
-    ## set the path for use it as argument for if statement
-    path_composite = get_image + 'composite.png'
-    ## condition is if there is only one image and that image is composite copy this image to output path
-    if len(os.listdir(get_image))== 1:
-        shutil.copy(path_composite, output_dir)
-        #print(os.listdir(get_image))
-    else:
-        ## drop image name composite.png
-        for list_image in [os.listdir(get_image)]:
-            if 'composite.png' in list_image:
-                list_image.remove('composite.png')
-                print(list_image)
-                ## sort image to get the older one
-                sorted_image = sorted(list_image, key=lambda x: datetime.strptime(extract_date(x), '%Y_%m_%d'))
-                path_image = get_image + sorted_image[0]
-                shutil.copy(path_image, output_dir)
-                #print(path_image)
-
-print(len(os.listdir(output_dir)))
+folders = train['example_path'].tolist()
+for f in folders:
+    images_path = os.path.join(data_dir, f, 'images', 'visible')
+    if os.path.isdir(images_path):
+        images = os.listdir(images_path)
+        images.sort()
+        # shutil.copy(
+        #     os.path.join(images_path, images[0]),
+        #     os.path.join(data_dir, f)
+        # )
+        print(images[0])
